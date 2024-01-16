e62bbaf8386800efc958e4f074b448ac44a09ffc
root <root@ip-185-96-163-91-62593.vps.hosted-by-mvps.net>
2024-01-16 18:12:55 +0100

    First version

--- src/platform.ts
+++ src/platform.ts
@@ -1,116 +1,105 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import fs from 'fs';
+import http from 'http';
+import express from 'express';
+import RED from 'node-red';
 
 /**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
+ * NodeRedHomebridgePlatform
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class NodeRedHomebridgePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
-
-  // this is used to track restored cached accessories
-  public readonly accessories: PlatformAccessory[] = [];
-
+  private port: number;
+  private storagePath: string;
+  private hapPin: string|null;
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
-
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
+    this.log = log;
+    this.log.info('Finished initializing platform:', config.name);
+    this.port = config.port;
+    this.storagePath = api.user.storagePath();
+    this.hapPin = null;
+    try {
+      const data = fs.readFileSync(api.user.configPath(), 'utf8');
+      const homebridgeConfig = JSON.parse(data);
+      this.hapPin = homebridgeConfig.bridge.pin;
+    } catch (err) {
+      this.log.error('Could not read main config file.', err);
+    }
     this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      this.nodeRed();
     });
   }
 
   /**
    * This function is invoked when homebridge restores cached accessories from disk at startup.
    * It should be used to setup event handlers for characteristics and update respective values.
+   *
+   *      !!! Since this platform does not produce any accessories, this function is not used.
    */
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
-
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
   }
 
   /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
+   * Start Node Red.
    */
-  discoverDevices() {
+  nodeRed() {
+    // Create an Express app
+    const app = express();
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    // Add a simple route for static content served from 'public'
+    //app.use("/",express.static("public"));
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    // Create a server
+    const server = http.createServer(app);
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    // Create the settings object - see default settings.js file for other options
+    const settings = {
+      flowFile: 'flows.json',
+      httpAdminRoot:'/red',
+      httpNodeRoot: '/api',
+      userDir:`${this.storagePath}/nodered`,
+      functionGlobalContext: { },    // enables global context
+    };
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    // On first startup, create a hb-conf node representing the local homebridge.
+    const flowFilePath = `${settings.userDir}/${settings.flowFile}`;
+    if (!fs.existsSync(flowFilePath)) {
+      this.log.info('Flows file did not exist => create one with a hb-config included');
+      const hbConfig = {
+        id: '60e46680800dce61',
+        type: 'hb-conf',
+        username: this.hapPin,
+        macAddress: '',
+      };
+      try {
+        fs.writeFileSync(flowFilePath, JSON.stringify([hbConfig]));
+      } catch (err) {
+        this.log.error('Problem writing to flows file: ', err);
       }
+    } else {
+      this.log.info('Flows file existed.');
     }
+
+    // Initialise the runtime with a server and settings
+    RED.init(server, settings);
+
+    // Serve the editor UI from /red
+    app.use(settings.httpAdminRoot, RED.httpAdmin);
+
+    // Serve the http nodes UI from /api
+    app.use(settings.httpNodeRoot, RED.httpNode);
+
+    server.listen(this.port);
+
+    // Start the runtime
+    RED.start();
+
   }
 }
